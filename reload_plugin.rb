8f185b7d6f1f14d087b8422228a1efbaf3de8bd1
Phạm Tiến Vinh <122341543+PhamTienVinh@users.noreply.github.com>
2025-07-23 11:48:23 +0700

    reload_plugin.rb

--- reload_plugin.rb
+++ reload_plugin.rb
@@ -1,26 +1,3 @@
-# scan_plugins.rb
-# March 28, 2010
-#
-# Description: scan a separate plugin directory and load scripts
-# automagically.
-#
-# To use, set PATH to some working directory and create your plugin
-# scripts there.  DirScanner will keep an eye on that directory and
-# load (i.e., execute) any scripts which appear there.  Because each
-# file will be executed as soon as it is changed, the Ruby files in
-# PATH should consist ONLY of SketchUp plugins!
-#
-# Copyright:  John Jacobsen, NPX Designs, Inc.
-#
-# Permission to use, copy, modify, and distribute this software for
-# any purpose and without fee is hereby granted, provided that the above
-# copyright notice appear in all copies.
-#
-# USE OF THIS SOFTWARE IS AT YOUR OWN RISK!
-# THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-
 require 'sketchup.rb'
 
 PATH = "E:/plug in vinh/vinharchi_tool"
